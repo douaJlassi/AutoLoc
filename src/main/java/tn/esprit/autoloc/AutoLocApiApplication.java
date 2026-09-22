fcd4cea0c659b226c7ff97a6267879e1cd5d8e32
douaJlassi <Doua.Jlassi@esprit.tn>
2026-09-22 14:06:43 +0100

    Initial commit - Spring Boot project

---
+++ src/main/java/tn/esprit/autoloc/AutoLocApiApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.autoloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoLocApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoLocApiApplication.class, args);
+    }
+
+}
